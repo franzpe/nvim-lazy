1f68c52aa4cf8bf216432bed7ee0ffd6144c1585
František Poboček <pobocekfrantisek@gmail.com>
2023-07-29 23:13:19 +0200

    alpha

---
+++ lua/plugins/vim-nightfly-colors.lua
@@ -0,0 +1,32 @@
+local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "nightfly",
+
+  callback = function()
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#081e2f" })
+    vim.api.nvim_set_hl(0, "LazyNormal", { bg = "NONE" })
+    vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#ffffff" })
+
+    -- lsp
+    vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", { bg = "#4b6479" })
+
+    -- notify
+    vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#000000" })
+
+    -- neotree
+    vim.api.nvim_set_hl(0, "NeoTreeDotfile", { fg = "#38507a" })
+  end,
+  group = custom_highlight,
+})
+
+vim.g.nightflyTransparent = true
+
+return {
+  {
+    "bluz71/vim-nightfly-colors",
+    name = "nightfly",
+    lazy = false,
+    priority = 1000,
+  },
+}
