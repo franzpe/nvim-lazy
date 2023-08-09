751b09c2ed6e7123877d9e1995df7405585c4f57
František Poboček <pobocekfrantisek@gmail.com>
2023-08-09 19:23:16 +0200

    remove unused

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,6 +7,7 @@ return {
         require("lazyvim.util").on_attach(function(_, buffer)
           vim.keymap.set("n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
           vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+          vim.keymap.set("n", "<leader>cu", ":TypescriptRemoveUnused<cr>", { desc = "Remove unused", buffer = buffer })
         end)
       end,
     },
