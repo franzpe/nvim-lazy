cfb357940bcd4b307f390a59c12d5c41951d74fb
František Poboček <pobocekfrantisek@gmail.com>
2023-09-03 12:02:31 +0200

    gopher

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -8,5 +8,17 @@ return {
 
     --multi cursor
     { "mg979/vim-visual-multi" },
+
+    --golang extras
+    {
+      "olexsmir/gopher.nvim",
+      keys = {
+        { "<leader>cgsj", "<cmd>GoTagAdd json<cr>", desc = "Add json struct tag" },
+        { "<leader>cgsy", "<cmd>GoTagAdd yaml<cr>", desc = "Add yaml struct tag" },
+      },
+      run = function(_, opts)
+        require("gopher").setup(opts)
+      end,
+    },
   },
 }
